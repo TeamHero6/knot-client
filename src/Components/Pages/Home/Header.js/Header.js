788a88411a457d67b7eecafcc6bfc94b2f03ce0a
Monir Hossain Abde Rabby <96588176+monirhabderabby@users.noreply.github.com>
2022-07-18 23:44:43 +0600

    added header

---
+++ src/Components/Pages/Home/Header.js/Header.js
@@ -0,0 +1,22 @@
+import React from "react";
+import FeaturedApp from "../FeaturedApp/FeaturedApp";
+
+const Header = () => {
+    return (
+        <div>
+            <div className="w-[100%] md:w-[80%] lg:w-[70%] mx-auto mb-32 px-2 md:px-0">
+                <h1 className="text-2xl md:text-5xl mt-12 font-semibold">
+                    Your Life's Work, Powered By Our Life's Work
+                </h1>
+                <p className="text-xm md:text-xl px-8 mt-3">
+                    Unique and powerful suite of software to run your entire
+                    business, brought to you by a company with the long term
+                    vision to transform the way you work.
+                </p>
+            </div>
+            <FeaturedApp />
+        </div>
+    );
+};
+
+export default Header;
--- src/Components/Pages/Home/Home.js
+++ src/Components/Pages/Home/Home.js
@@ -1,10 +1,12 @@
 import React from "react";
 import Navbar from "../../Shared/Navbar/Navbar";
+import Header from "./Header.js/Header";
 
 const Home = () => {
     return (
         <div>
             <Navbar />
+            <Header />
         </div>
     );
 };
