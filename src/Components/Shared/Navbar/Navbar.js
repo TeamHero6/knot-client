cdee1350dce65a2f0e4b97a4db24ddfa1fc446f1
Monir Hossain Abde Rabby <96588176+monirhabderabby@users.noreply.github.com>
2022-07-18 22:55:12 +0600

    added Navbar

---
+++ src/Components/Shared/Navbar/Navbar.js
@@ -0,0 +1,119 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const Navbar = () => {
+    return (
+        <div className="px-4 md:px-8 lg:px-12">
+            <div class="navbar bg-base-100">
+                <div class="navbar-start">
+                    <div class="dropdown">
+                        <label tabindex="0" class="btn btn-ghost lg:hidden">
+                            <svg
+                                xmlns="http://www.w3.org/2000/svg"
+                                class="h-5 w-5"
+                                fill="none"
+                                viewBox="0 0 24 24"
+                                stroke="currentColor"
+                            >
+                                <path
+                                    stroke-linecap="round"
+                                    stroke-linejoin="round"
+                                    stroke-width="2"
+                                    d="M4 6h16M4 12h8m-8 6h16"
+                                />
+                            </svg>
+                        </label>
+                        <ul
+                            tabindex="0"
+                            class="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52"
+                        >
+                            <li>
+                                <Link to="/customers">Customers</Link>
+                            </li>
+                            <li>
+                                <Link to="/support" className="px-4">
+                                    Support
+                                </Link>
+                            </li>
+                            <li>
+                                <Link to="/contactSales" className="px-4">
+                                    Contact Sales
+                                </Link>
+                            </li>
+                            <li>
+                                <Link to="/events" className="px-4">
+                                    Events
+                                </Link>
+                            </li>
+                            <li>
+                                <Link
+                                    to="/accessYourApps"
+                                    className="px-4 text-green-500"
+                                >
+                                    Access your apps
+                                </Link>
+                            </li>
+                        </ul>
+                    </div>
+                    <p className="text-white text-3xl">
+                        <span className="bg-red-500 p-1">K</span>
+                        <span className="p-1 bg-green-500">N</span>
+                        <span className="p-1 bg-blue-500">O</span>
+                        <span className="p-1 bg-yellow-500">T</span>
+                    </p>
+                </div>
+                <div class="navbar-end">
+                    <div className="hidden lg:flex">
+                        <Link to="/customers" className="px-4">
+                            Customers
+                        </Link>
+                        <Link to="/support" className="px-4">
+                            Support
+                        </Link>
+                        <Link to="/contactSales" className="px-4">
+                            Contact Sales
+                        </Link>
+                        <Link to="/events" className="px-4">
+                            Events
+                        </Link>
+                        <Link
+                            to="/accessYourApps"
+                            className="px-4 text-green-500"
+                        >
+                            Access your apps
+                        </Link>
+                    </div>
+                    <div class="dropdown dropdown-end px-4">
+                        <label
+                            tabindex="0"
+                            class="btn btn-ghost btn-circle avatar"
+                        >
+                            <div class="w-10 rounded-full">
+                                <img src="https://placeimg.com/80/80/people" />
+                            </div>
+                        </label>
+                        <ul
+                            tabindex="0"
+                            class="mt-3 p-2 shadow menu menu-compact dropdown-content bg-base-100 rounded-box w-52"
+                        >
+                            <li>
+                                <a class="justify-between">
+                                    Profile
+                                    <span class="badge">New</span>
+                                </a>
+                            </li>
+                            <li>
+                                <a>Settings</a>
+                            </li>
+                            <li>
+                                <a>Logout</a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Navbar;
