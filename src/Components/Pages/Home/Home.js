80cac9f2f21eff00935405626798818c1607ee02
Monir Hossain Abde Rabby <96588176+monirhabderabby@users.noreply.github.com>
2022-07-19 22:22:05 +0600

    intro and footer added by al habib

--- src/Components/Pages/Home/Home.js
+++ src/Components/Pages/Home/Home.js
@@ -1,19 +1,17 @@
 import React from "react";
 import Footer from "../../Shared/Footer/Footer";
 
-
 import Navbar from "../../Shared/Navbar/Navbar";
 import Header from "./Header.js/Header";
 import Intro from "./Intro";
-
 
 const Home = () => {
     return (
         <div>
             <Navbar />
             <Header />
-            <Intro/>
-            <Footer/>
+            <Intro />
+            <Footer />
         </div>
     );
 };
