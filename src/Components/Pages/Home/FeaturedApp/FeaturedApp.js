89e9f316a184e9ba3107ab6cbd8f7287af2b8c39
Monir Hossain Abde Rabby <96588176+monirhabderabby@users.noreply.github.com>
2022-07-20 23:25:33 +0600

    making featured app eye catching

--- src/Components/Pages/Home/FeaturedApp/FeaturedApp.js
+++ src/Components/Pages/Home/FeaturedApp/FeaturedApp.js
@@ -26,17 +26,23 @@ const FeaturedApp = () => {
                 <div className="text-start">
                     <div className="grid grid-cols-1 lg:grid-cols-2">
                         <div className="text-center sm:border-t-[1px] md:border-t-[0px] md:border-r-[1px] md:border-b-[1px] py-12 border-gray-200 w-[100%] mx-auto">
-                            <h3>Mail</h3>
-                            <p className="mb-4">Secure Business email</p>
-                            <Link
-                                to="/learnMore"
-                                className="bg-transparent border-[1px] border-sky-400 text-sky-400 font-semibold px-2 lg:px-4 py-2 hover:text-white hover:bg-sky-400 transform  duration-[.7s] transition-all"
-                            >
-                                LEARN MORE
-                            </Link>
+                            <div>
+                                <section>
+                                    <h3>Sales</h3>
+                                    <p className="mb-4">
+                                        Secure Business email
+                                    </p>
+                                    <Link
+                                        to="/learnMore"
+                                        className="bg-transparent border-[1px] border-sky-400 text-sky-400 font-semibold px-2 lg:px-4 py-2 hover:text-white hover:bg-sky-400 transform  duration-[.7s] transition-all"
+                                    >
+                                        LEARN MORE
+                                    </Link>
+                                </section>
+                            </div>
                         </div>
                         <div className="text-center  md:border-b-[1px] py-12 border-gray-200">
-                            <h3>Mail</h3>
+                            <h3>Finance</h3>
                             <p className="mb-4">Secure Business email</p>
                             <Link
                                 to="/learnMore"
@@ -46,7 +52,7 @@ const FeaturedApp = () => {
                             </Link>
                         </div>
                         <div className="text-center border-r-[1px] py-12 border-gray-200">
-                            <h3>Mail</h3>
+                            <h3>Marketing</h3>
                             <p className="mb-4">Secure Business email</p>
                             <Link
                                 to="/learnMore"
@@ -56,7 +62,7 @@ const FeaturedApp = () => {
                             </Link>
                         </div>
                         <div className="text-center py-12 border-gray-200">
-                            <h3>Mail</h3>
+                            <h3>Live</h3>
                             <p className="mb-4">Secure Business email</p>
                             <Link
                                 to="/learnMore"
