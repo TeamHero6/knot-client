fd5293eb6de85c30599a023e0abb02469d2c565c
mobashirul-alam <mobashirulalamseam@gmail.com>
2022-07-20 07:41:34 +0600

    app icon and how to work section completed

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Home from "./Components/Pages/Home/Home";
 
 function App() {
     return (
-        <div className="App">
+        <div>
             <Routes>
                 <Route path="/" element={<Home />}></Route>
             </Routes>
--- src/Components/Pages/Home/Home.js
+++ src/Components/Pages/Home/Home.js
@@ -2,12 +2,14 @@ import React from "react";
 import Navbar from "../../Shared/Navbar/Navbar";
 import AllKnotApp from "./AllKnotApp/AllKnotApp";
 import Header from "./Header.js/Header";
+import Promotion from "./Promotion/Promotion";
 
 const Home = () => {
     return (
         <div>
             <Navbar />
             <Header />
+            <Promotion />
             <AllKnotApp />
         </div>
     );
