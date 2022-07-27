fbdf70b73a9d86c91b2eaa9799da1cb7c8d80df0
Monir Hossain Abde Rabby <96588176+monirhabderabby@users.noreply.github.com>
2022-07-27 10:51:18 +0600

    structure added connect your apps components

--- src/Components/Pages/AccessYourApps/AccessYourApps.js
+++ src/Components/Pages/AccessYourApps/AccessYourApps.js
@@ -1,7 +1,47 @@
 import React from "react";
+import { FaSearch } from "react-icons/fa";
+import financeIcon from "../../../Assets/icons/financereport.png";
+import marketingIcon from "../../../Assets/icons/marketing-automation.png";
+import salesTSLogo from "../../../Assets/icons/sales2.png";
+import background from "../../../Assets/images/networkMash.jpg";
+import logo from "../../../Assets/logo/KnotLogo.png";
+import AccessAppsCard from "./AccessAppsCard/AccessAppsCard";
 
 const AccessYourApps = () => {
-    return <div></div>;
+    const products = [
+        { id: 1, name: "Sales", link: "/salesTS", icon: salesTSLogo },
+        { id: 2, name: "Marketing", link: "/marketingTS", icon: marketingIcon },
+        { id: 2, name: "Finance", link: "/financeTS", icon: financeIcon },
+        { id: 1, name: "Sales", link: "/salesTS", icon: salesTSLogo },
+        { id: 2, name: "Marketing", link: "/marketingTS", icon: marketingIcon },
+        { id: 2, name: "Finance", link: "/financeTS", icon: financeIcon },
+    ];
+    return (
+        <div
+            style={{
+                backgroundImage: `url(${background})`,
+                backgroundPosition: "center",
+                backgroundRepeat: "no-repeat",
+            }}
+            className="min-h-screen"
+        >
+            <div className="flex relative justify-end md:justify-center items-center h-10 border-[0.5px] border-gray-400">
+                <img src={logo} className="w-12 absolute left-4" alt="" />
+                <FaSearch className="mr-4 text-gray-400 hover:text-cyan-400 duration-300 " />
+                <input
+                    type="text"
+                    name="search"
+                    className="h-full outline-none cursor-pointer text-gray-400 bg-transparent"
+                    placeholder="Search"
+                />
+            </div>
+            <div className="grid grid-cols-1 md:grid-cols-3 gap-4 lg:w-2/3 mx-auto mt-4">
+                {products.map((product) => (
+                    <AccessAppsCard {...{ product }} />
+                ))}
+            </div>
+        </div>
+    );
 };
 
 export default AccessYourApps;
