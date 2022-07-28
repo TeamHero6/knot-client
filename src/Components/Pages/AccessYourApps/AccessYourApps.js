92b6f8b2ee22868eecce994d8fffdfd7a521e4c1
Monir Hossain Abde Rabby <96588176+monirhabderabby@users.noreply.github.com>
2022-07-28 20:48:20 +0600

    added filtering on access your apps

--- src/Components/Pages/AccessYourApps/AccessYourApps.js
+++ src/Components/Pages/AccessYourApps/AccessYourApps.js
@@ -1,22 +1,74 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { FaSearch } from "react-icons/fa";
 import { useNavigate } from "react-router-dom";
-import financeIcon from "../../../Assets/icons/financereport.png";
-import marketingIcon from "../../../Assets/icons/marketing-automation.png";
-import salesTSLogo from "../../../Assets/icons/sales2.png";
+import finance from "../../../Assets/icons/financereport.png";
+import marketing from "../../../Assets/icons/marketing-automation.png";
+import sales from "../../../Assets/icons/sales2.png";
 import background from "../../../Assets/images/networkMash.jpg";
 import logo from "../../../Assets/logo/KnotLogo.png";
 import AccessAppsCard from "./AccessAppsCard/AccessAppsCard";
 
 const AccessYourApps = () => {
+    const [allProducts, setProducts] = useState([]);
+    const [filterResult, setFilterResult] = useState([]);
+
     const products = [
-        { id: 1, name: "Sales", link: "/salesTS", icon: salesTSLogo },
-        { id: 2, name: "Marketing", link: "/marketingTS", icon: marketingIcon },
-        { id: 2, name: "Finance", link: "/financeTS", icon: financeIcon },
-        { id: 1, name: "Sales", link: "/salesTS", icon: salesTSLogo },
-        { id: 2, name: "Marketing", link: "/marketingTS", icon: marketingIcon },
-        { id: 2, name: "Finance", link: "/financeTS", icon: financeIcon },
+        {
+            id: 1,
+            name: "Sales",
+            link: "/salesTS",
+            icon: sales,
+        },
+        {
+            id: 2,
+            name: "Marketing",
+            link: "/marketingTS",
+            icon: marketing,
+        },
+        {
+            id: 3,
+            name: "Finance",
+            link: "/financeTS",
+            icon: finance,
+        },
+        {
+            id: 4,
+            name: "Sales",
+            link: "/salesTS",
+            icon: sales,
+        },
+        {
+            id: 5,
+            name: "Marketing",
+            link: "/marketingTS",
+            icon: marketing,
+        },
+        {
+            id: 6,
+            name: "Finance",
+            link: "/financeTS",
+            icon: finance,
+        },
     ];
+    useEffect(() => {
+        setFilterResult(products);
+    }, []);
+
+    const handleFilter = (e) => {
+        const searchTerm = e.target.value;
+        if (searchTerm < 0) {
+            setFilterResult(products);
+        } else {
+            const regex = new RegExp(`${searchTerm}`, "gi");
+            const matchs = products.filter((product) => {
+                if (product.name.match(regex)) {
+                    return product;
+                }
+            });
+            setFilterResult(matchs);
+        }
+    };
+
     const navigate = useNavigate();
     return (
         <div
@@ -40,12 +92,17 @@ const AccessYourApps = () => {
                     name="search"
                     className="h-full outline-none cursor-pointer text-gray-400 bg-transparent"
                     placeholder="Search"
+                    onChange={handleFilter}
                 />
             </div>
             <div className="grid grid-cols-1 md:grid-cols-3 gap-4 lg:w-2/3 mx-auto mt-4">
-                {products.map((product) => (
-                    <AccessAppsCard {...{ product }} />
-                ))}
+                {filterResult
+                    ? filterResult?.map((product) => (
+                          <AccessAppsCard {...{ product }} />
+                      ))
+                    : products?.map((product) => (
+                          <AccessAppsCard {...{ product }} />
+                      ))}
             </div>
         </div>
     );
