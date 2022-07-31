1b683e820ec4877c22ff1f493931235f8c0b9d13
Ahsanul Hasan Kabbo <kabboahsanulhasan@gmail.com>
2022-08-01 00:39:31 +0600

    leave request add

--- src/Components/Pages/Leave/LeaveRequest/LeaveRequest.js
+++ src/Components/Pages/Leave/LeaveRequest/LeaveRequest.js
@@ -1,104 +1,48 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 const LeaveRequest = () => {
+    const [userinfo, setUserinfo] = useState([]);
+
+    useEffect(() => {
+        fetch('http://localhost:5000/leaveData')
+            .then(res => res.json())
+            .then(data => setUserinfo(data))
+    }, [])
+    // console.log(userinfo)
     return (
         <div>
             <div className='my-10'>
-                        <p className='text-xl text-color md:ml-5'>Leave Report</p>
-                        <p className='text-xl text-center mb-3'>Leave Request</p>
-                        <table className='text-center responsive bg-[#EEEEEE] w-full'>
-                            <tr>
-                                <th>Name</th>
-                                <th>Department Name</th>
-                                <th>Designation</th>
-                                <th>Leave Date</th>
-                                <th>Reason For Leave</th>
-                                <th>Leave Request</th>
-                                <th>Approval</th>
-                            </tr>
-                            <tr>
-                                <td>Kamal</td>
-                                <td>Sales</td>
-                                <td>Jr. Executive</td>
-                                <td>12 Jul to 22 to 14 Jul 22</td>
-                                <td>Sick</td>
-                                <td>Approve by team leader</td>
-                                <td>Accept</td>
-                            </tr>
-                            <tr>
-                                <td>Jamal</td>
-                                <td>Finance</td>
-                                <td>Executive</td>
-                                <td>12 Jul to 22 to 14 Jul 22</td>
-                                <td>Sick</td>
-                                <td>Reject</td>
-                                <td>Reject</td>
-                            </tr>
-                            <tr>
-                                <td>Kamal</td>
-                                <td>Sales</td>
-                                <td>Jr. Executive</td>
-                                <td>12 Jul to 22 to 14 Jul 22</td>
-                                <td>Sick</td>
-                                <td>Approve by team leader</td>
-                                <td>Accept</td>
-                            </tr>
-                            <tr>
-                                <td>Jamal</td>
-                                <td>Finance</td>
-                                <td>Executive</td>
-                                <td>12 Jul to 22 to 14 Jul 22</td>
-                                <td>Sick</td>
-                                <td>Reject</td>
-                                <td>Reject</td>
-                            </tr>
-                            <tr>
-                                <td>Kamal</td>
-                                <td>Sales</td>
-                                <td>Jr. Executive</td>
-                                <td>12 Jul to 22 to 14 Jul 22</td>
-                                <td>Sick</td>
-                                <td>Approve by team leader</td>
-                                <td>Accept</td>
-                            </tr>
-                            <tr>
-                                <td>Jamal</td>
-                                <td>Finance</td>
-                                <td>Executive</td>
-                                <td>12 Jul to 22 to 14 Jul 22</td>
-                                <td>Sick</td>
-                                <td>Reject</td>
-                                <td>Reject</td>
-                            </tr>
-                            <tr>
-                                <td>Kamal</td>
-                                <td>Sales</td>
-                                <td>Jr. Executive</td>
-                                <td>12 Jul to 22 to 14 Jul 22</td>
-                                <td>Sick</td>
-                                <td>Approve by team leader</td>
-                                <td>Accept</td>
-                            </tr>
-                            <tr>
-                                <td>Jamal</td>
-                                <td>Finance</td>
-                                <td>Executive</td>
-                                <td>12 Jul to 22 to 14 Jul 22</td>
-                                <td>Sick</td>
-                                <td>Reject</td>
-                                <td>Reject</td>
-                            </tr>
-                            <tr>
-                                <td>Jamal</td>
-                                <td>Finance</td>
-                                <td>Executive</td>
-                                <td>12 Jul to 22 to 14 Jul 22</td>
-                                <td>Sick</td>
-                                <td>Reject</td>
-                                <td>Reject</td>
-                            </tr>
-                        </table>
-                    </div>
+                <p className='text-xl text-color md:ml-5'>Leave Report</p>
+                <p className='text-xl text-center mb-3'>Leave Request</p>
+                <table className='text-center responsive bg-[#EEEEEE] w-full'>
+                    <thead>
+                        <tr>
+                            <th>Name</th>
+                            <th>Dept.</th>
+                            <th>Designation</th>
+                            <th>Leave Date</th>
+                            <th>Reason For Leave</th>
+                            <th>Leave Request</th>
+                            <th>Approval</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {
+                            userinfo?.map(user =>
+                                <tr>
+                                    <td>{user?.Name}</td>
+                                    <td>{user?.dep}</td>
+                                    <td>{user?.Designation}</td>
+                                    <td>{user?.leave_date}</td>
+                                    <td>{user?.leave}</td>
+                                    <td>Details</td>
+                                    <td>Edit</td>
+                                </tr>
+                            )
+                        }
+                    </tbody>
+                </table>
+            </div>
         </div>
     );
 };
