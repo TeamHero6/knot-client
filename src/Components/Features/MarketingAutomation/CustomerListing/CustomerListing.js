882bea3240aeacdedc2cf1e26e6672dff4c081ca
mobashirul-alam <mobashirulalamseam@gmail.com>
2022-08-08 18:15:53 +0600

    check

--- src/Components/Features/MarketingAutomation/CustomerListing/CustomerListing.js
+++ src/Components/Features/MarketingAutomation/CustomerListing/CustomerListing.js
@@ -1,9 +1,41 @@
+import { useQuery } from '@tanstack/react-query';
 import React from 'react';
+import Loader from '../../../Shared/Loader/Loader';
 
 const CustomerListing = () => {
+
+    const { data: newsletterMail, isLoading } = useQuery({
+        queryKey: 'newsletterMail',
+        queryFn: () => fetch('http://localhost:5000/newsletterMail').then(res => res.json())
+    });
+
+    if (isLoading) {
+        return <Loader></Loader>;
+    }
     return (
         <div>
-            <h1>This is customer listing</h1>
+            <h1 className="text-center text-3xl my-2">Customer Collection</h1>
+            <div className='mx-5 mt-3'>
+                <div className="overflow-x-auto">
+                    <table className="mx-auto table table-zebra w-1/2">
+                        <thead>
+                            <tr>
+                                <th>SL.</th>
+                                <th>E-MAIL</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {
+                                newsletterMail?.map((mail, index) => <tr key={mail._id}>
+                                    <th>{index + 1}</th>
+                                    <td>{mail.email}</td>
+                                </tr>)
+                            }
+
+                        </tbody>
+                    </table>
+                </div>
+            </div>
         </div>
     );
 };
