f64ed72257cca3f4738ad3e69b1d64ddd4024a4c
Monir Hossain Abde Rabby <96588176+monirhabderabby@users.noreply.github.com>
2022-08-19 12:08:25 +0600

    ready for live

--- src/Components/Shared/Loader/Loader.js
+++ src/Components/Shared/Loader/Loader.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const Loader = ({ children }) => {
     return (
-        <div className="flex items-center space-x-2 animate-bounce">
+        <div className="flex items-center justify-center w-full min-h-screen space-x-2 animate-bounce">
             <div className="w-8 h-8 bg-blue-400 rounded-full"></div>
             <div className="w-8 h-8 bg-green-400 rounded-full"></div>
             <div className="w-8 h-8 bg-black rounded-full"></div>
