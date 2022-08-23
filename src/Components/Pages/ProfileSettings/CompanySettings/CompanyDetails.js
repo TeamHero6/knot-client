8b523be6a19d44078423db35d14c15c8477fec2a
Monir Hossain Abde Rabby <96588176+monirhabderabby@users.noreply.github.com>
2022-08-23 09:18:54 +0600

    filtering feature implemented in company setting page

--- src/Components/Pages/ProfileSettings/CompanySettings/CompanyDetails.js
+++ src/Components/Pages/ProfileSettings/CompanySettings/CompanyDetails.js
@@ -6,6 +6,7 @@ import EmployeeAccessModal from "./EmployeeAccessModal";
 
 const CompanyDetails = () => {
     const [open, setOpen] = useState(false);
+    const [searchTerm, setSearchTerm] = useState("");
     const loggerInfo = useSelector((state) => state.auth.loggerInfo);
     const { companyName, companyLogo } = loggerInfo;
 
@@ -60,8 +61,8 @@ const CompanyDetails = () => {
     return (
         <div>
             <section className="my-6 px-6 w-full bg-white">
-                <section className="flex justify-between items-center">
-                    <div className="flex items-center px-6 md:px-12 py-3">
+                <section className="flex items-center">
+                    <div className="flex flex-auto items-center px-6 md:px-12 py-3">
                         <img
                             src={companyLogo}
                             className="w-6 h-4 mr-3"
@@ -71,9 +72,17 @@ const CompanyDetails = () => {
                             {companyName}
                         </h3>
                     </div>
+                    <div className="flex-auto">
+                        <input
+                            type="text"
+                            className="border-[0.5px] max-w-64 outline-none px-2 py-1 rounded text-sm text-gray-400"
+                            placeholder="Search by Name"
+                            onChange={(e) => setSearchTerm(e.target.value)}
+                        />
+                    </div>
                     <label
                         for="addEmployee"
-                        class="modal-button mx-6 md:mx-12 flex px-3 py-1 rounded-lg drop-shadow-md items-center border-[0.5px]"
+                        class="modal-button flex-none md:w-auto mx-6 md:mx-12 flex px-1 md:px-3 py-1 rounded-lg drop-shadow-md items-center border-[0.5px]"
                         onClick={() => setOpen(!open)}
                     >
                         <svg
@@ -90,8 +99,8 @@ const CompanyDetails = () => {
                     </label>
                 </section>
                 <hr />
-                <section className="w-full mt-3 px-6">
-                    <table class="min-w-max w-full table-auto">
+                <section className="w-full mt-3 px-6 overflow-x-auto">
+                    <table class="min-w-max w-full table-auto overflow-hidden">
                         <thead>
                             <tr class="bg-gray-100 text-gray-600 uppercase text-sm leading-normal">
                                 <th class="py-3 px-6 text-left">Name</th>
@@ -100,97 +109,103 @@ const CompanyDetails = () => {
                             </tr>
                         </thead>
                         <tbody class="text-gray-600 text-sm font-light">
-                            {allEmployees?.map((em) => {
-                                return (
-                                    <>
-                                        <tr>
-                                            <td class="py-3 px-3 text-left whitespace-nowrap">
-                                                <div class="flex items-center">
-                                                    <img
-                                                        class="w-8 h-8 rounded-full"
-                                                        src={em?.userPhoto}
-                                                        alt=""
-                                                    />
-                                                    <div class="flex flex-col ml-2">
-                                                        <span class="text-sm">
-                                                            {em?.name}
-                                                        </span>
-                                                        <span class="text-xs">
-                                                            {em?.email}
-                                                        </span>
+                            {allEmployees
+                                ?.filter((em) => {
+                                    return em.name
+                                        .toLowerCase()
+                                        .includes(searchTerm.toLowerCase());
+                                })
+                                .map((em) => {
+                                    return (
+                                        <>
+                                            <tr>
+                                                <td class="py-3 px-3 text-left whitespace-nowrap">
+                                                    <div class="flex items-center">
+                                                        <img
+                                                            class="w-8 h-8 rounded-full"
+                                                            src={em?.userPhoto}
+                                                            alt=""
+                                                        />
+                                                        <div class="flex flex-col ml-2">
+                                                            <span class="text-sm">
+                                                                {em?.name}
+                                                            </span>
+                                                            <span class="text-xs">
+                                                                {em?.email}
+                                                            </span>
+                                                        </div>
                                                     </div>
-                                                </div>
-                                            </td>
-                                            <td className="py-3 px-6 text-left whitespace-nowrap">
-                                                {em?.role}
-                                            </td>
-                                            <td class="py-3 px-6 text-center">
-                                                <div class="flex item-center justify-center">
-                                                    <div class="w-4 mr-2 transform hover:text-cyan-400 hover:scale-110">
-                                                        <svg
-                                                            xmlns="http://www.w3.org/2000/svg"
-                                                            fill="none"
-                                                            viewBox="0 0 24 24"
-                                                            stroke="currentColor"
+                                                </td>
+                                                <td className="py-3 px-6 text-left whitespace-nowrap">
+                                                    {em?.role}
+                                                </td>
+                                                <td class="py-3 px-6 text-center">
+                                                    <div class="flex item-center justify-center">
+                                                        <div class="w-4 mr-2 transform hover:text-cyan-400 hover:scale-110">
+                                                            <svg
+                                                                xmlns="http://www.w3.org/2000/svg"
+                                                                fill="none"
+                                                                viewBox="0 0 24 24"
+                                                                stroke="currentColor"
+                                                            >
+                                                                <path
+                                                                    stroke-linecap="round"
+                                                                    stroke-linejoin="round"
+                                                                    stroke-width="2"
+                                                                    d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"
+                                                                />
+                                                                <path
+                                                                    stroke-linecap="round"
+                                                                    stroke-linejoin="round"
+                                                                    stroke-width="2"
+                                                                    d="M2.458 12C3.732 7.943 7.523 5 12 5c4.478 0 8.268 2.943 9.542 7-1.274 4.057-5.064 7-9.542 7-4.477 0-8.268-2.943-9.542-7z"
+                                                                />
+                                                            </svg>
+                                                        </div>
+                                                        <div class="w-4 mr-2 transform hover:text-green-400 hover:scale-110">
+                                                            <svg
+                                                                xmlns="http://www.w3.org/2000/svg"
+                                                                fill="none"
+                                                                viewBox="0 0 24 24"
+                                                                stroke="currentColor"
+                                                            >
+                                                                <path
+                                                                    stroke-linecap="round"
+                                                                    stroke-linejoin="round"
+                                                                    stroke-width="2"
+                                                                    d="M15.232 5.232l3.536 3.536m-2.036-5.036a2.5 2.5 0 113.536 3.536L6.5 21.036H3v-3.572L16.732 3.732z"
+                                                                />
+                                                            </svg>
+                                                        </div>
+                                                        <div
+                                                            class="w-4 mr-2 transform hover:text-red-400 hover:scale-110"
+                                                            onClick={() =>
+                                                                deleteEmployeeHandler(
+                                                                    em?._id,
+                                                                    em?.name
+                                                                )
+                                                            }
                                                         >
-                                                            <path
-                                                                stroke-linecap="round"
-                                                                stroke-linejoin="round"
-                                                                stroke-width="2"
-                                                                d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"
-                                                            />
-                                                            <path
-                                                                stroke-linecap="round"
-                                                                stroke-linejoin="round"
-                                                                stroke-width="2"
-                                                                d="M2.458 12C3.732 7.943 7.523 5 12 5c4.478 0 8.268 2.943 9.542 7-1.274 4.057-5.064 7-9.542 7-4.477 0-8.268-2.943-9.542-7z"
-                                                            />
-                                                        </svg>
+                                                            <svg
+                                                                xmlns="http://www.w3.org/2000/svg"
+                                                                fill="none"
+                                                                viewBox="0 0 24 24"
+                                                                stroke="currentColor"
+                                                            >
+                                                                <path
+                                                                    stroke-linecap="round"
+                                                                    stroke-linejoin="round"
+                                                                    stroke-width="2"
+                                                                    d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16"
+                                                                />
+                                                            </svg>
+                                                        </div>
                                                     </div>
-                                                    <div class="w-4 mr-2 transform hover:text-green-400 hover:scale-110">
-                                                        <svg
-                                                            xmlns="http://www.w3.org/2000/svg"
-                                                            fill="none"
-                                                            viewBox="0 0 24 24"
-                                                            stroke="currentColor"
-                                                        >
-                                                            <path
-                                                                stroke-linecap="round"
-                                                                stroke-linejoin="round"
-                                                                stroke-width="2"
-                                                                d="M15.232 5.232l3.536 3.536m-2.036-5.036a2.5 2.5 0 113.536 3.536L6.5 21.036H3v-3.572L16.732 3.732z"
-                                                            />
-                                                        </svg>
-                                                    </div>
-                                                    <div
-                                                        class="w-4 mr-2 transform hover:text-red-400 hover:scale-110"
-                                                        onClick={() =>
-                                                            deleteEmployeeHandler(
-                                                                em?._id,
-                                                                em?.name
-                                                            )
-                                                        }
-                                                    >
-                                                        <svg
-                                                            xmlns="http://www.w3.org/2000/svg"
-                                                            fill="none"
-                                                            viewBox="0 0 24 24"
-                                                            stroke="currentColor"
-                                                        >
-                                                            <path
-                                                                stroke-linecap="round"
-                                                                stroke-linejoin="round"
-                                                                stroke-width="2"
-                                                                d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16"
-                                                            />
-                                                        </svg>
-                                                    </div>
-                                                </div>
-                                            </td>
-                                        </tr>
-                                    </>
-                                );
-                            })}
+                                                </td>
+                                            </tr>
+                                        </>
+                                    );
+                                })}
                         </tbody>
                     </table>
                 </section>
