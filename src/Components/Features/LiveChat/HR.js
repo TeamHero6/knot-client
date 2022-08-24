17cda56499fe34fb6c97bd5d813f2c1ed51316db
alhabib911 <abdullahalhabib100@gmail.com>
2022-08-24 15:51:10 +0600

    live chat update

--- src/Components/Features/LiveChat/HR.js
+++ src/Components/Features/LiveChat/HR.js
@@ -1,11 +1,9 @@
 import moment from "moment";
 import React, { useState } from "react";
-import { Link, useParams } from "react-router-dom";
-import useHRChat from "./hooks/useHRChat";
-import useLoginUser from "./hooks/useLoginUser";
-import useUserInfo from "./hooks/useUserInfo";
 import HRChat from "./HRChat";
 import "./LiveChat.css";
+import { useSelector } from "react-redux";
+import { FiSend } from 'react-icons/fi';
 
 const HR = () => {
     const [singChatDetail, setSingleChatDetail] = useState({});
@@ -14,10 +12,11 @@ const HR = () => {
         e.preventDefault();
         const chat = e.target.chat.value;
         const userName = e.target.userName.value;
+        const userPhoto = e.target.userPhoto.value;
         const time = e.target.time.value;
 
-        const hrchat = { chat, userName, time };
-        console.log(hrChat);
+        const hrchat = { chat, userName, time, userPhoto };
+        console.log(hrchat);
 
         fetch("https://knot-business-solution-server.herokuapp.com/hrchat", {
             method: "POST",
@@ -34,18 +33,107 @@ const HR = () => {
                 }
             });
     };
-    const { id } = useParams();
-    const [hrChat] = useHRChat();
-    const [userInfo] = useUserInfo();
-    const [loginUser] = useLoginUser();
-    // console.log(loginUser);
 
-    const singleLoginUser = loginUser.find((product) => loginUser._id == id);
-    // console.log(singleLoginUser);
+    const loggerInfo = useSelector((state) => state.auth.loggerInfo);
+    if (loggerInfo) {
+        // console.log(loggerInfo);
+    }
 
     return (
         <div>
-            <section className="relative chatBox lg:w-full mx-auto bg-white shadow-gray-300 border shadow-sm rounded pt-5 px-5 md:w-9/12 sm:w-11/12 sm:mx-auto">
+            <section className="h-[87vh]  bg-white">
+                {/* <div className="flex justify-between px-10 py-5 bg-custom-light-gray shadow-sm">
+                    <div className="font-semibold">
+                        HR Management Chat Board
+                    </div>
+                    <div className="flex items-center gap-2">
+                        <div class="avatar placeholder">
+                            <div class="bg-neutral-focus text-neutral-content rounded-full w-10">
+                                <img src={loggerInfo?.userPhoto} alt="" />
+                            </div>
+                        </div>
+                        <div>
+                            <p className="font-semibold">{loggerInfo?.name}</p>
+                        </div>
+                    </div>
+                </div>
+                <hr /> */}
+
+                <div className="flex justify-between  ">
+                    <div className="w-3/12 h-[74vh] bg-white">
+                        <div className="flex items-center gap-2 rounded bg-custom-light-gray shadow-sm py-1 my-2 mx-6 justify-center">
+                            <div class="avatar placeholder">
+                                <div class="bg-neutral-focus text-neutral-content rounded-full w-10">
+                                    <img src={loggerInfo?.userPhoto} alt="" />
+                                </div>
+                            </div>
+                            <div>
+                                <p>{loggerInfo?.name}</p>
+                                <p><small className="italic text-gray-400">{loggerInfo?.role}</small></p>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="border-l-2 pr-1"></div>
+                    <div className="w-9/12">
+                        <div className="h-[72vh] overflow-y-scroll ">
+                            <HRChat setSingleItemDetail={singChatDetail}></HRChat>
+                        </div>
+                        <div>
+                        <hr className="mx-8 " />
+                            <form
+                                onSubmit={handleHRChat}
+                                className="flex gap-5 bg-white pt-4 pr-36"
+                            >
+                                <input
+                                    type="hidden"
+                                    name="userName"
+                                    id=""
+                                    value={loggerInfo?.name}
+                                />
+                                <input
+                                    type="hidden"
+                                    name="userPhoto"
+                                    id=""
+                                    value={loggerInfo?.userPhoto}
+                                />
+                                <input
+                                    type="hidden"
+                                    name="time"
+                                    id=""
+                                    value={moment().format(
+                                        "MMMM Do YYYY, h:mm:ss a"
+                                    )}
+                                />
+                                <div className="flex items-center">
+                                    <div>
+                                        <textarea
+                                            className="border-none pl-3 ml-10 rounded outline-none"
+                                            name="chat"
+                                            id=""
+                                            cols="100"
+                                            rows="2"
+                                            placeholder="Write your message..."
+                                            required
+                                        ></textarea>
+                                    </div>
+                                    <div className="ml-10">
+                                        <button className="text-2xl">
+                                            <div class="avatar placeholder ">
+                                                <div class=" bg-blue-500 text-neutral-content rounded-full w-12">
+                                                    <span><FiSend /></span>
+                                                </div>
+                                            </div>
+                                            
+                                        </button>
+                                    </div>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+
+            </section>
+            {/* <section className="relative chatBox lg:w-full mx-auto bg-white shadow-gray-300 border shadow-sm rounded pt-5 px-5 md:w-9/12 sm:w-11/12 sm:mx-auto">
                 <div className="flex justify-end"></div>
                 <div className="flex gap-8  ">
                     <div className="groupName">
@@ -123,7 +211,7 @@ const HR = () => {
                         </div>
                     </div>
                 </div>
-            </section>
+            </section> */}
         </div>
     );
 };
