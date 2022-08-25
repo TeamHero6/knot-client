65ba5db8590b2229ae6df50e8bd1554a00c201b5
alhabib911 <abdullahalhabib100@gmail.com>
2022-08-26 04:12:46 +0600

    circular update

--- src/Components/Features/HRDashboard/HRVacancy/HrVacancy.js
+++ src/Components/Features/HRDashboard/HRVacancy/HrVacancy.js
@@ -5,6 +5,7 @@ import { BiPlus } from "react-icons/bi";
 import { IoIosAddCircleOutline } from "react-icons/io";
 import { Link } from "react-router-dom";
 import { toast } from "react-toastify";
+import HRCircularCard from "./HRCircularCard";
 
 const HrVacancy = () => {
     const [show, setShow] = useState(false);
@@ -76,6 +77,40 @@ const HrVacancy = () => {
                     <div className="">
                         <form onSubmit={handleSubmit(onSubmit)}>
                             <section className="lg:w-7/12 mx-auto bg-white shadow-gray-300 border shadow-md rounded py-12 px-5 mt-10 md:w-9/12 sm:w-11/12 sm:mx-auto">
+                                <div className="md:flex items-center">
+                                    <label
+                                        className="font-bold w-52"
+                                        htmlFor="name"
+                                    >
+                                        Publish Date:{" "}
+                                    </label>{" "}
+                                    <br />
+                                    <input
+                                        className="py-2 pl-3 w-full my-1 border border-gray-300 bg-slate-50 rounded outline-none "
+                                        type="date"
+                                        {...register("Job_Publish_Date")}
+                                        id=""
+                                        placeholder="Job Publish Date"
+                                    />
+                                    <br />
+                                </div>
+                                <div className="md:flex items-center">
+                                    <label
+                                        className="font-bold w-52"
+                                        htmlFor="name"
+                                    >
+                                        Last Apply Date:{" "}
+                                    </label>{" "}
+                                    <br />
+                                    <input
+                                        className="py-2 pl-3 w-full my-1 border border-gray-300 bg-slate-50 rounded outline-none "
+                                        type="date"
+                                        {...register("Last_Date_Apply")}
+                                        id=""
+                                        placeholder="Last Date for Apply:"
+                                    />
+                                    <br />
+                                </div>
                                 <div className="flex flex-row gap-5">
                                     <input
                                         className="py-2 pl-3 w-full my-1 border border-gray-300 bg-slate-50 rounded outline-none "
@@ -101,6 +136,22 @@ const HrVacancy = () => {
                                         placeholder="Position"
                                     />
                                 </div>
+                                <div className="flex flex-row gap-5">
+                                    <input
+                                        className="py-2 pl-3 w-full my-1 border border-gray-300 bg-slate-50 rounded outline-none "
+                                        type="text"
+                                        {...register("Salary_Range")}
+                                        id=""
+                                        placeholder="Salary Range"
+                                    />
+                                    <input
+                                        className="py-2 pl-3 w-full my-1 border border-gray-300 bg-slate-50 rounded outline-none "
+                                        type="text"
+                                        {...register("Number_openings")}
+                                        id=""
+                                        placeholder="Number of openings"
+                                    />
+                                </div>
 
                                 <div className="flex flex-row gap-5">
                                     <textarea
@@ -111,60 +162,17 @@ const HrVacancy = () => {
                                         placeholder="About the job"
                                     />
                                 </div>
-                                <div className="flex flex-row gap-5">
-                                    <textarea
-                                        className="py-2 pl-3 w-full my-1 border border-gray-300 bg-slate-50 rounded outline-none "
-                                        type="text"
-                                        {...register("Skill_required:")}
-                                        id=""
-                                        placeholder="Skill required:"
-                                    />
-                                </div>
-                                <div className="flex flex-row gap-5">
-                                    <input
-                                        className="py-2 pl-3 w-full my-1 border border-gray-300 bg-slate-50 rounded outline-none "
-                                        type="text"
-                                        {...register("Salary_Range")}
-                                        id=""
-                                        placeholder="Salary Range"
-                                    />
-                                </div>
-                                <div className="flex flex-row gap-5">
-                                    <input
-                                        className="py-2 pl-3 w-full my-1 border border-gray-300 bg-slate-50 rounded outline-none "
-                                        type="text"
-                                        {...register("Number_openings")}
-                                        id=""
-                                        placeholder="Number of openings"
-                                    />
-                                </div>
-                                <div className="flex flex-row gap-5">
-                                    <input
-                                        className="py-2 pl-3 w-full my-1 border border-gray-300 bg-slate-50 rounded outline-none "
-                                        type="text"
-                                        {...register("Job_Publish_Date")}
-                                        id=""
-                                        placeholder="Job Publish Date"
-                                    />
-                                </div>
-                                <div className="flex flex-row gap-5">
-                                    <input
-                                        className="py-2 pl-3 w-full my-1 border border-gray-300 bg-slate-50 rounded outline-none "
-                                        type="text"
-                                        {...register("Last_Date_Apply")}
-                                        id=""
-                                        placeholder="Last Date for Apply:"
-                                    />
-                                </div>
 
-                                <div className="lg:flex justify-between md:flex pt-2">
+
+                                <div className="flex justify-center mt-2">
                                     <button
-                                        className="flex items-center gap-2 bg-blue-600 py-2 px-6 text-white font-bold rounded  hover:bg-white hover:text-blue-600 hover:border-blue-600 hover: shadow-blue-300 hover: shadow-sm"
-                                        type="subimt"
+                                        type="submit"
+                                        className="flex items-center gap-2 bg-blue-600 py-2 px-6 text-white font-bold rounded  hover:bg-white hover:text-blue-600 hover:outline-1 hover:border hover:border-blue-600 hover: shadow-blue-300 hover: shadow-sm"
                                     >
                                         <AiFillSave />
                                         Save
-                                    </button>
+                                    </button>{" "}
+                                    <br />
                                 </div>
                             </section>
                         </form>
@@ -175,7 +183,7 @@ const HrVacancy = () => {
             </div>
             <div className="my-5">
                 <h3 className="text-[#0182be] text-2xl">Circular</h3>
-                <div className="mx-auto w-full rounded-lg my-3 ">
+                {/* <div className="mx-auto w-full rounded-lg my-3 ">
                     <div class="rounded-none w-full mx-auto ">
                         <table class="shadow-2xl border-2 border-cyan-300 w-full mx-auto my-12 text-base overflow-hidden">
                             <thead className="text-white bg-cyan-500 border-b border-cyan-100">
@@ -286,6 +294,14 @@ const HrVacancy = () => {
                             </div>
                         </div>
                     </div>
+                </div> */}
+
+                <div className='flex justify-between gap-5'>
+                    {
+                        carcular.slice(0, 20).map(circular => <HRCircularCard
+                            circular={circular}
+                        ></HRCircularCard>)
+                    }
                 </div>
             </div>
         </div>
