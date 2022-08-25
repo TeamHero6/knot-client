7d02c430cc93da871ffc8b804e12cd86187dd800
alhabib911 <abdullahalhabib100@gmail.com>
2022-08-25 17:24:10 +0600

    award warning ui update

---
+++ src/Components/Features/HRDashboard/EmployeeManagement/HRAwardCard.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const HRAwardCard = (props) => {
+    const {AwardDate, awardTitle, successMessage, employeeEmail} =props.award
+    return (
+        <div className='bg-white shadow-gray-300 px-5 py-3 rounded-md shadow-sm'>
+            <p><span className='font-semibold'>Email:</span> {employeeEmail}</p>
+            <p className='font-semibold font-xl'>{awardTitle}</p>
+            <p><span className='font-semibold'>Appreciate:</span> <small className='italic'>{successMessage}</small></p>
+            <p className='text-right italic '><span>Date:</span> <small className='text-gray-400'>{AwardDate}</small></p>
+
+        </div>
+    );
+};
+
+export default HRAwardCard;
