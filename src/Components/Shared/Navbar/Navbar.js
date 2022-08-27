b64b44e0a2f40ae6b35f0fb434438fefa07e87e8
Monir Hossain Abde Rabby <96588176+monirhabderabby@users.noreply.github.com>
2022-08-27 13:09:09 +0600

    notification icon add and ready a redux state for manage this notificatio ison

--- src/Components/Shared/Navbar/Navbar.js
+++ src/Components/Shared/Navbar/Navbar.js
@@ -6,12 +6,18 @@ import { Link, NavLink, useNavigate } from "react-router-dom";
 import logo from "../../../Assets/logo/KnotLogo.png";
 import auth from "../../../firebase.init";
 import { logout } from "../../../Redux/Auth/authAction";
+import { NotifiyStatusUpdate } from "../../../Redux/notification/NotificationAction";
+import Loader from "../Loader/Loader";
 
 const Navbar = () => {
     const [user, loading] = useAuthState(auth);
     const [userProfile, setuserprofile] = useState("");
     const dispatch = useDispatch();
     const navigate = useNavigate();
+
+    // get redux state
+    const isOpen = useSelector((state) => state.notification.isOpen);
+    console.log("notification status is ", isOpen);
     //use Info from Redux
     const authInfo = useSelector((state) => state.auth);
     useEffect(() => {
@@ -27,9 +33,14 @@ const Navbar = () => {
         navigate("/");
     };
 
+    // handle Notification handler
+    const handleNotificationHandler = () => {
+        dispatch(NotifiyStatusUpdate(!isOpen));
+    };
+
     //Sign out user
     if (loading) {
-        return;
+        return <Loader />;
     }
     return (
         <div className="relative ">
@@ -181,6 +192,31 @@ const Navbar = () => {
                         >
                             Access your apps
                         </NavLink>
+                        {user && (
+                            <NavLink
+                                to="/notification"
+                                onClick={handleNotificationHandler}
+                                className={({ isActive }) =>
+                                    isActive
+                                        ? " text-white px-2 py-2 rounded-md mx-1"
+                                        : " rounded-full px-2 py-2 mx-1"
+                                }
+                            >
+                                <span className="relative">
+                                    <sup className="absolute right-0">2</sup>
+                                    <svg
+                                        xmlns="http://www.w3.org/2000/svg"
+                                        width="16"
+                                        height="16"
+                                        fill="currentColor"
+                                        class=""
+                                        viewBox="0 0 16 16"
+                                    >
+                                        <path d="M8 16a2 2 0 0 0 2-2H6a2 2 0 0 0 2 2zM8 1.918l-.797.161A4.002 4.002 0 0 0 4 6c0 .628-.134 2.197-.459 3.742-.16.767-.376 1.566-.663 2.258h10.244c-.287-.692-.502-1.49-.663-2.258C12.134 8.197 12 6.628 12 6a4.002 4.002 0 0 0-3.203-3.92L8 1.917zM14.22 12c.223.447.481.801.78 1H1c.299-.199.557-.553.78-1C2.68 10.2 3 6.88 3 6c0-2.42 1.72-4.44 4.005-4.901a1 1 0 1 1 1.99 0A5.002 5.002 0 0 1 13 6c0 .88.32 4.2 1.22 6z" />
+                                    </svg>
+                                </span>
+                            </NavLink>
+                        )}
                         <div>
                             {user ? (
                                 <div className="dropdown dropdown-end">
--- src/store/rootReducer.js
+++ src/store/rootReducer.js
@@ -1,6 +1,8 @@
 import { combineReducers } from "redux";
 import authReducer from "../Redux/Auth/AuthReducer";
+import NotificationReducer from "../Redux/notification/NotificationReducer";
 
 export const rootReducer = combineReducers({
     auth: authReducer,
+    notification: NotificationReducer,
 });
