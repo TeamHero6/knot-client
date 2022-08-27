0660b7935935a9534356a4335c53a77748d74a16
Monir Hossain Abde Rabby <96588176+monirhabderabby@users.noreply.github.com>
2022-08-28 00:00:06 +0600

    loader status update

--- src/Components/Pages/Home/Home.js
+++ src/Components/Pages/Home/Home.js
@@ -1,8 +1,8 @@
 import React, { lazy } from "react";
+import Navbar from "../../Shared/Navbar/Navbar";
 import AccessibleAnywhere from "./AccessibleAnywhere/AccessibleAnywhere";
 import Client from "./Client/Client";
 import Newsletter from "./Newsletter/Newsletter";
-const Navbar = lazy(() => import("../../Shared/Navbar/Navbar"));
 const Header = lazy(() => import("./Header.js/Header"));
 const Promotion = lazy(() => import("./Promotion/Promotion"));
 const Footer = lazy(() => import("../../Shared/Footer/Footer"));
