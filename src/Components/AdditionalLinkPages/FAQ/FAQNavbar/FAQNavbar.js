2796b33f9f27e2494da2d2cbfe71fecb09208f9c
Monir Hossain Abde Rabby <96588176+monirhabderabby@users.noreply.github.com>
2022-08-29 10:15:59 +0600

    faq header dynamic username set

--- src/Components/AdditionalLinkPages/FAQ/FAQNavbar/FAQNavbar.js
+++ src/Components/AdditionalLinkPages/FAQ/FAQNavbar/FAQNavbar.js
@@ -1,12 +1,15 @@
 import { signOut } from "firebase/auth";
 import React from "react";
-import { useAuthState } from "react-firebase-hooks/auth";
-import { Link } from "react-router-dom";
+import { useSelector } from "react-redux";
+import { Link, useNavigate } from "react-router-dom";
 import logo from "../../../../Assets/logo/KnotLogo.png";
 import auth from "../../../../firebase.init";
 
 const FAQNavbar = () => {
-    const [user, loading, error] = useAuthState(auth);
+    const loggerInfo = useSelector((state) => state.auth?.loggerInfo);
+
+    const navigate = useNavigate();
+
     return (
         <div className=" md:px-8 lg:px-12 bg-cyan-400">
             <div className="navbar ">
@@ -34,14 +37,6 @@ const FAQNavbar = () => {
                         >
                             <li>
                                 <Link
-                                    to="/FAQ"
-                                    className="text-cyan-400 font-bold mx-2"
-                                >
-                                    Help Center
-                                </Link>
-                            </li>
-                            <li>
-                                <Link
                                     to="/FAQ/Guides"
                                     className="text-cyan-400 mx-2"
                                 >
@@ -49,12 +44,12 @@ const FAQNavbar = () => {
                                 </Link>
                             </li>
                             <li>
-                                <Link
-                                    to="/contactus"
-                                    className="text-cyan-400 border-2 border-cyan-400 px-3 py-1 rounded-full mx-2 hover:bg-cyan-400 duration-300 hover:border-cyan-400 outline-none hover:text-white"
+                                <a
+                                    href="mailto:support.knot@mail.com"
+                                    className="text-gray-200 border-2 border-gray-200 px-3 py-1 rounded-full mx-2 hover:bg-white duration-300 hover:border-white outline-none hover:text-cyan-400"
                                 >
-                                    Contact us
-                                </Link>
+                                    Contact Us
+                                </a>
                             </li>
                         </ul>
                     </div>
@@ -64,22 +59,20 @@ const FAQNavbar = () => {
                             src={logo}
                             className="w-20 p-1 md:w-20 md:p-2"
                             alt="logo"
+                            onClick={() => navigate("/")}
                         />
                     </div>
                     {/*Nav Link*/}
                     <div className="hidden lg:block">
-                        <Link to="/FAQ" className="text-white font-bold mx-2">
-                            Help Center
-                        </Link>
                         <Link to="/FAQ/Guides" className="text-gray-200 mx-2">
                             Guides
                         </Link>
-                        <Link
-                            to="/contactus"
+                        <a
+                            href="mailto:support.knot@mail.com"
                             className="text-gray-200 border-2 border-gray-200 px-3 py-1 rounded-full mx-2 hover:bg-white duration-300 hover:border-white outline-none hover:text-cyan-400"
                         >
-                            Contact us
-                        </Link>
+                            Contact Us
+                        </a>
                     </div>
                 </div>
                 <div className="navbar-end">
@@ -97,13 +90,14 @@ const FAQNavbar = () => {
                             className="mt-3 p-2 shadow menu menu-compact dropdown-content bg-base-100 rounded-box w-52"
                         >
                             <li>
-                                <a className="justify-between">
+                                <button
+                                    className="justify-between"
+                                    onClick={() =>
+                                        navigate("/settings/profile")
+                                    }
+                                >
                                     Profile
-                                    <span className="badge">New</span>
-                                </a>
-                            </li>
-                            <li>
-                                <a>Settings</a>
+                                </button>
                             </li>
                             <li>
                                 <p onClick={() => signOut(auth)}>Logout</p>
