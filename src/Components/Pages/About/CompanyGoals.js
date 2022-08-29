019312247365423aac813ec4a1cd87b36ac82d3d
alhabib911 <abdullahalhabib100@gmail.com>
2022-08-29 20:13:42 +0600

    about page update

--- src/Components/Pages/About/CompanyGoals.js
+++ src/Components/Pages/About/CompanyGoals.js
@@ -5,21 +5,17 @@ const CompanyGoals = () => {
     return (
         <section className="flex flex-col md:flex-row-reverse md:justify-center md:align-center w-full min-h-screen md:px-8 lg:px-12 mt-12">
             <div className="text-left w-[100%] flex flex-col justify-center  md:w-1/2 px-4">
-                <h2 className="text-4xl font-bold tracking-wide mb-4">
-                    We Handle All Your Problem Business
+                <h2 className="text-2xl font-bold tracking-wide mb-4 text-blue-500">
+                    Mission
                 </h2>
                 <p>
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                    Molestias alias odio, molestiae, cum dolore consequatur
-                    nihil natus itaque suscipit id excepturi in accusamus veniam
-                    quam magnam. Officiis perspiciatis velit provident, a
-                    laborum autem ab iste possimus sapiente cumque perferendis
-                    ex quidem quam tenetur pariatur, rem sint vitae. Sed
-                    distinctio nihil voluptatem perspiciatis ipsum et error unde
-                    maxime architecto, minus eius aut, sint accusantium veniam
-                    ipsa. Autem impedit ab cupiditate quidem quae quia veniam
-                    similique quam molestiae quos, harum dolores a assumenda aut
-                    expedita, iste iusto voluptas reprehenderit inventore
+                    Our developers are passionate to eliminate all types of challenges a businessman could face in the starting of his business or at any stage of the business. We do that by adding new technology to our project. We work to make our features extraordinary and make them user-friendly. Our research is ongoing on every possible sector of a business. Every day we join to break down business challenges and make the business process as simple as it should be.
+                </p>
+                <h2 className="text-2xl font-bold tracking-wide mb-4 text-orange-500 mt-5">
+                    Vision
+                </h2>
+                <p>
+                    Our vision is to create a simple, user-friendly, flexible, customizable, and most effective solution for any type of business whether it is small or giant. A world where no one needs to think about how to start his dream business or how to manage everything. We will provide every type of tool that a businessman wants to make his dream true and succeed.
                 </p>
             </div>
             <div className="w-[100%] md:w-1/2 mt-6 md:mt-0 flex justify-center items-center">
