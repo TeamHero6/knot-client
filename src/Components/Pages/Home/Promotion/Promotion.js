d99492449bd455424a13839264d5e9236ff34830
Monir Hossain Abde Rabby <96588176+monirhabderabby@users.noreply.github.com>
2022-08-30 10:27:52 +0600

    added homepagecircleIcon

--- src/Components/Pages/Home/Promotion/Promotion.js
+++ src/Components/Pages/Home/Promotion/Promotion.js
@@ -1,5 +1,5 @@
 import React from "react";
-import homePageCircleIcon from "../../../../Assets/images/homePageCircleIcon.svg";
+import homePageCircleIcon from "../../../../Assets/images/homePageCircleIcon (1).png";
 import knotIntroThumbnail from "../../../../Assets/images/introVideoThumb.png";
 
 const Promotion = () => {
@@ -7,11 +7,7 @@ const Promotion = () => {
         <div className="my-12 md:my-20">
             <div className="md:flex justify-center md:gap-4 md:mx-20 lg:mx-32">
                 <div className="md:w-1/2 p-8">
-                    <img
-                        className="w-full"
-                        src={homePageCircleIcon}
-                        alt=""
-                    />
+                    <img className="w-full" src={homePageCircleIcon} alt="" />
                 </div>
                 <div className="md:ml-4 md:w-1/2 p-8 md:my-auto text-center">
                     <div>
@@ -19,7 +15,9 @@ const Promotion = () => {
                             One Stop Business Solution
                         </h3>
                         <p className="text-lg mt-2 text-gray-400">
-                            Adjustable for any business. Accessible from any of your favorite devices and any of your favorite places.
+                            Adjustable for any business. Accessible from any of
+                            your favorite devices and any of your favorite
+                            places.
                         </p>
                     </div>
                     <div className="mt-6">
